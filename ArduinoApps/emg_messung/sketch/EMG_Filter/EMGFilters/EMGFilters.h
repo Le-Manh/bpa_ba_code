7efd7ea7419141dc65463ba0963eee948ac374e5
Manh Le <Manh.le@stud.hawk.de>
2026-04-23 09:45:09 +0000

    changed the library to have filter state as instance data:
    this has to be done to have individual filters in every sensor.
    reference: https://github.com/oymotion/EMGFilters/pull/4

--- ArduinoApps/emg_messung/sketch/EMG_Filter/EMGFilters/EMGFilters.h
+++ ArduinoApps/emg_messung/sketch/EMG_Filter/EMGFilters/EMGFilters.h
@@ -32,11 +32,42 @@
 #ifndef _EMGFILTERS_H
 #define _EMGFILTERS_H
 
-#define SAMPLE_FREQ_500HZ 500
-#define SAMPLE_FREQ_1000HZ 1000
+enum NOTCH_FREQUENCY { NOTCH_FREQ_50HZ = 50, NOTCH_FREQ_60HZ = 60 };
 
-#define NOTCH_FREQ_50HZ 50
-#define NOTCH_FREQ_60HZ 60
+enum SAMPLE_FREQUENCY { SAMPLE_FREQ_500HZ = 500, SAMPLE_FREQ_1000HZ = 1000 };
+
+enum FILTER_TYPE {
+    FILTER_TYPE_LOWPASS = 0,
+    FILTER_TYPE_HIGHPASS,
+};
+
+class FILTER_2nd {
+  private:
+    // second-order filter
+    float states[2];
+    float num[3];
+    float den[3];
+
+  public:
+    void init(FILTER_TYPE ftype, int sampleFreq);
+
+    float update(float input);
+};
+
+class FILTER_4th {
+  private:
+    // fourth-order filter
+    // cascade two 2nd-order filters
+    float states[4];
+    float num[6];
+    float den[6];
+    float gain;
+
+  public:
+    void init(int sampleFreq, int humFreq);
+
+    float update(float input);
+};
 
 // \brief EMGFilter provides an anti-hum notch filter to filter out 50HZ or
 //        60HZ power line noise, a lowpass filter to filter out signals above
@@ -56,8 +87,8 @@ class EMGFilters {
     // default
     // \remark The filter won't work and just bypass input if sampleFreq or
     //         notchFreq isn't specified properly.
-    void init(int sampleFreq,
-              int  notchFreq,
+    void init(SAMPLE_FREQUENCY sampleFreq,
+              NOTCH_FREQUENCY  notchFreq,
               bool             enableNotchFilter    = true,
               bool             enableLowpassFilter  = true,
               bool             enableHighpassFilter = true);
@@ -67,12 +98,15 @@ class EMGFilters {
     int update(int inputValue);
 
   private:
-    int              m_sampleFreq;
-    int              m_notchFreq;
+    SAMPLE_FREQUENCY m_sampleFreq;
+    NOTCH_FREQUENCY  m_notchFreq;
     bool             m_bypassEnabled;
     bool             m_notchFilterEnabled;
     bool             m_lowpassFilterEnabled;
     bool             m_highpassFilterEnabled;
+    FILTER_2nd       LPF;
+    FILTER_2nd       HPF;
+    FILTER_4th       AHF;
 };
 
 #endif
