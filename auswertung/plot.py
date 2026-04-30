3d9f44c9179753ea11eacc874bbb91f2fac9f735
Manh Le <Manh.le@stud.hawk.de>
2026-04-30 11:12:48 +0200

    Reading of csv test data

--- auswertung/plot.py
+++ auswertung/plot.py
@@ -1,16 +1,6 @@
-# This is a sample Python script.
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+csv_data = pd.read_csv("../ArduinoApps/emg_messung/python/messdaten/Test.csv")
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
