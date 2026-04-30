8d8b51330a2f8b77963a6ffdd197a418472c236d
Manh Le <Manh.le@stud.hawk.de>
2026-04-30 11:50:41 +0200

    splitting of dataframe based on sensor

--- auswertung/plot.py
+++ auswertung/plot.py
@@ -3,4 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 csv_data = pd.read_csv("../ArduinoApps/emg_messung/python/messdaten/Test.csv")
+splitted_data = list()
 
+for i in range(4):
+    splitted_data.append(csv_data[csv_data['sensor'] == i][['Aktueller Finger','Wert']]) #split Messung per sensor
+
+
+
+
+
+
+print
